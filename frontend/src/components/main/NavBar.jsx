ddbfb248a4d86772b73ed7325546a38908039809
CipiVlad <102032121+CipiVlad@users.noreply.github.com>
2023-06-16 17:18:16 +0200

    sidebar in progress

--- frontend/src/components/main/NavBar.jsx
+++ frontend/src/components/main/NavBar.jsx
@@ -1,5 +1,5 @@
-import { AppBar, Typography, Toolbar, Tabs, Tab, Box, Grid } from '@mui/material';
 
+import { AppBar, Typography, Toolbar, Tabs, Tab, Box, Grid, IconButton } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import SettingsIcon from '@mui/icons-material/Settings';
 import MailIcon from '@mui/icons-material/Mail';
@@ -9,15 +9,38 @@ import MenuBookIcon from '@mui/icons-material/MenuBook';
 import ShareIcon from '@mui/icons-material/Share';
 import PrintIcon from '@mui/icons-material/Print';
 import MenuIcon from '@mui/icons-material/Menu'
-
+import SideBar from './SideBar';
+import React from 'react';
 const NavBar = () => {
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
     return (
         <>
             <AppBar>
                 <Toolbar>
-                    <Typography>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        onClick={toggleDrawer("left", true)}
+                    >
                         <MenuIcon />
-                    </Typography>
+                    </IconButton>
                     <Box sx={{ flexGrow: 2 }}>
                         <Grid container spacing={6}
 
@@ -26,7 +49,7 @@ const NavBar = () => {
                             alignItems="center"
                         >
                             <Grid item xs={1}>
-                                <Tab icon={<ArrowBackIcon />} iconPosition="start" />
+                                <Tab icon={<ArrowBackIcon />} iconPosition="end" />
                             </Grid>
                             <Grid item xs={1}>
                                 <Tab icon={<SettingsIcon />} iconPosition="end" />
@@ -54,6 +77,7 @@ const NavBar = () => {
 
                 </Toolbar>
             </AppBar>
+            <SideBar state={state} setState={setState} toggleDrawer={toggleDrawer}></SideBar>
         </>
     )
 }
--- frontend/src/components/main/SideBar.jsx
+++ frontend/src/components/main/SideBar.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import HandshakeIcon from '@mui/icons-material/Handshake';
 
-const SideBar = () => {
+export default function SideBar({ state, setState, toggleDrawer }) {
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Angebote', 'Mietverträge', 'Rechnungen', 'Vorlagen', 'Statistik'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {/* < HistoryEduIcon />
+                                <HandshakeIcon /> */}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            {/* <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List> */}
+        </Box>
+    );
+
     return (
-        <div>SideBar</div>
-    )
+        <div>
+            {['left'].map((anchor) => (
+                <React.Fragment key={anchor}>
+                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                    <Drawer
+                        anchor={anchor}
+                        open={state[anchor]}
+                        onClose={toggleDrawer(anchor, false)}
+                    >
+                        {list(anchor)}
+                    </Drawer>
+                </React.Fragment>
+            ))}
+        </div>
+    );
 }
-
-export default SideBar
