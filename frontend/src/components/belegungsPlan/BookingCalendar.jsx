52264a1e41b6d9c43b7c5599b07901c4690f91b8
Ciprian Vlad <supercoder.cipi@gmail.com>
2023-06-23 14:25:52 +0200

    react-bootstrap variation

--- frontend/src/components/belegungsPlan/BookingCalendar.jsx
+++ frontend/src/components/belegungsPlan/BookingCalendar.jsx
@@ -1,23 +1,264 @@
-import React, { useState } from 'react';
-import moment from 'moment';
-import { Container, Row, Col, Button, Modal, Form } from 'react-bootstrap';
+// import React, { useState } from 'react';
+// import moment from 'moment';
+// import { Container, Row, Col, Button, Modal, Form } from 'react-bootstrap';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
+// const BookingCalendar = () => {
+//     const [selectedDate, setSelectedDate] = useState(null);
+//     const [bookingData, setBookingData] = useState({});
+//     const [showModal, setShowModal] = useState(false);
+
+//     // const handleContainerClick = (containerNumber) => {
+//     //     setSelectedDate(containerNumber);
+//     // };
+
+//     const handleBookingSubmit = (formData) => {
+//         const { startDate, endDate, name, phoneNumber, price } = formData;
+//         const newBookingData = {
+//             ...bookingData,
+//             [selectedDate]: {
+//                 startDate,
+//                 endDate,
+//                 name,
+//                 phoneNumber,
+//                 price,
+//             },
+//         };
+//         setBookingData(newBookingData);
+//         setSelectedDate(null);
+//         setShowModal(false);
+//     };
+
+//     const renderCalendarContainer = (containerNumber) => {
+//         const isSelected = selectedDate === containerNumber;
+//         const containerStyle = {
+//             width: '100%',
+//             height: '90px',
+//             backgroundColor: '#999',
+//             display: 'flex',
+//             alignItems: 'center',
+//             justifyContent: 'flex-start',
+//             position: 'relative',
+//             marginBottom: '5px',
+//         };
+
+//         const circleStyle = {
+//             width: '50px',
+//             height: '50px',
+//             borderRadius: '50%',
+//             display: 'flex',
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//             backgroundColor: isSelected ? 'lightblue' : 'white',
+//             marginLeft: '10px',
+//             cursor: 'pointer'
+//         };
+
+//         const textStyle = {
+//             fontWeight: isSelected ? 'bold' : 'normal',
+//         };
+
+//         const bookingLineStyle = {
+//             width: '0',
+//             height: '15px',
+//             borderRadius: '6px',
+//             backgroundColor: 'green',
+//             visibility: bookingData[containerNumber] ? 'visible' : 'hidden',
+//             left: 'calc(10px + 25px)',
+//             transition: 'width 0.3s',
+//         };
+
+//         if (bookingData[containerNumber]) {
+//             const startDate = moment(bookingData[containerNumber].startDate, 'YYYY-MM-DD');
+//             const endDate = moment(bookingData[containerNumber].endDate, 'YYYY-MM-DD');
+//             const bookingDays = endDate.diff(startDate, 'days') + 1;
+//             const totalDaysInWeek = 7; // Gesamtanzahl der Tage in der Woche
+//             const bookingGaps = bookingDays - 1; // Anzahl der Lücken zwischen den gebuchten Tagen
+//             const bookingWidth = (bookingGaps / totalDaysInWeek) * 100; // Berechnung der Breite in Prozent
+//             bookingLineStyle.width = `${bookingWidth}%`;
+//         }
+
+
+
+//         const bookingDataContent = bookingData[containerNumber] ? (
+//             <div style={{ position: 'absolute', bottom: '-20px' }}>
+//                 <p>
+//                     Name: {bookingData[containerNumber].name} - Telefonnummer:{' '}
+//                     {bookingData[containerNumber].phoneNumber} - Preis: {bookingData[containerNumber].price}
+//                 </p>
+//             </div>
+//         ) : null;
+
+//         const handleContainerClick = () => {
+//             setSelectedDate(containerNumber);
+//             setShowModal(true);
+//         };
+
+//         const bookingStartDate = bookingData[containerNumber] ? moment(bookingData[containerNumber].startDate, 'YYYY-MM-DD') : null;
+//         const bookingEndDate = bookingData[containerNumber] ? moment(bookingData[containerNumber].endDate, 'YYYY-MM-DD') : null;
+
+//         let bookingLineWidth = '0';
+
+//         if (bookingStartDate && bookingEndDate) {
+//             const bookingDuration = bookingEndDate.diff(bookingStartDate, 'days') + 1;
+//             bookingLineWidth = `${bookingDuration * 50}px`;
+//         }
+
+//         return (
+//             <Row key={containerNumber}>
+//                 <div style={containerStyle}>
+//                     <div style={circleStyle} onClick={handleContainerClick}>
+//                         <p style={textStyle}>{containerNumber}</p>
+//                     </div>
+//                     {bookingDataContent}
+//                     {bookingData[containerNumber] && (
+//                         <div
+//                             style={{
+//                                 ...bookingLineStyle,
+//                                 width: bookingLineWidth,
+//                             }}
+//                         ></div>
+//                     )}
+//                 </div>
+//             </Row>
+//         );
+//     };
+
+
+//     const renderStatusBars = () => {
+//         const statusBars = [];
+//         let currentStatus = '';
+//         let isBooked = false;
+
+//         for (let i = 1; i <= 7; i++) {
+//             const containerNumber = i;
+//             const containerKey = `container-${containerNumber}`;
+
+//             if (bookingData[containerKey]) {
+//                 if (!isBooked) {
+//                     currentStatus = `${bookingData[containerKey].name} - ${bookingData[containerKey].phoneNumber} - ${bookingData[containerKey].price}`;
+//                     isBooked = true;
+//                 }
+//             } else {
+//                 if (isBooked) {
+//                     statusBars.push(
+//                         <div
+//                             key={containerKey}
+//                             className="status-bar"
+//                             style={{
+//                                 width: `${((i - 1) * 1) + 50}px`,
+//                             }}
+//                         ></div>
+//                     );
+//                     isBooked = false;
+//                 }
+//             }
+//         }
+
+//         if (isBooked) {
+//             statusBars.push(
+//                 <div
+//                     key="container-8"
+//                     className="status-bar"
+//                     style={{
+//                         width: `${(7 * 50) + 10}px`,
+//                     }}
+//                 ></div>
+//             );
+//         }
+
+//         return statusBars;
+//     };
+
+//     const renderBookingForm = () => {
+//         if (!selectedDate) {
+//             return null;
+//         }
+
+//         const handleSubmit = (event) => {
+//             event.preventDefault();
+//             const formData = new FormData(event.target);
+//             const startDate = moment(formData.get('startDate'), 'YYYY-MM-DD');
+//             const endDate = moment(formData.get('endDate'), 'YYYY-MM-DD');
+//             const name = formData.get('name');
+//             const phoneNumber = formData.get('phoneNumber');
+//             const price = formData.get('price');
+//             handleBookingSubmit({ startDate, endDate, name, phoneNumber, price });
+//         };
+
+//         return (
+//             <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+//                 <Modal.Header closeButton>
+//                     <Modal.Title>Buchung</Modal.Title>
+//                 </Modal.Header>
+//                 <Modal.Body>
+//                     <Form onSubmit={handleSubmit}>
+//                         <Form.Group controlId="startDate">
+//                             <Form.Label>Anreisedatum</Form.Label>
+//                             <Form.Control type="date" name="startDate" required />
+//                         </Form.Group>
+
+//                         <Form.Group controlId="endDate">
+//                             <Form.Label>Abreisedatum</Form.Label>
+//                             <Form.Control type="date" name="endDate" required />
+//                         </Form.Group>
+
+//                         <Form.Group controlId="name">
+//                             <Form.Label>Name</Form.Label>
+//                             <Form.Control type="text" name="name" required />
+//                         </Form.Group>
+
+//                         <Form.Group controlId="phoneNumber">
+//                             <Form.Label>Telefonnummer</Form.Label>
+//                             <Form.Control type="tel" name="phoneNumber" required />
+//                         </Form.Group>
+
+//                         <Form.Group controlId="price">
+//                             <Form.Label>Preis</Form.Label>
+//                             <Form.Control type="number" name="price" required />
+//                         </Form.Group>
+
+//                         <Button type="submit" variant="primary">
+//                             Buchen
+//                         </Button>
+//                     </Form>
+//                 </Modal.Body>
+//             </Modal>
+//         );
+//     };
+
+//     const calendarContainers = [...Array(7)].map((_, index) => {
+//         const containerNumber = index + 1;
+//         return renderCalendarContainer(containerNumber);
+//     });
+
+//     return (
+//         <Container fluid>
+//             <Row>{calendarContainers}</Row>
+//             <Row>{renderStatusBars()}</Row>
+//             {renderBookingForm()}
+//         </Container>
+//     );
+// };
+
+// export default BookingCalendar;
+
+import React, { useState } from "react";
+import moment from "moment";
+import { Container, Row, Button, Modal, Form } from "react-bootstrap";
+// Importieren Sie die CSS-Datei
+import "./BookingCalendar.css"
 const BookingCalendar = () => {
     const [selectedDate, setSelectedDate] = useState(null);
     const [bookingData, setBookingData] = useState({});
     const [showModal, setShowModal] = useState(false);
 
-    const handleContainerClick = (date) => {
-        setSelectedDate(date);
-    };
-
     const handleBookingSubmit = (formData) => {
         const { startDate, endDate, name, phoneNumber, price } = formData;
         const newBookingData = {
             ...bookingData,
-            [startDate]: {
+            [selectedDate]: {
                 startDate,
                 endDate,
                 name,
@@ -30,27 +271,18 @@ const BookingCalendar = () => {
         setShowModal(false);
     };
 
-    const renderCalendarContainer = (date, index) => {
-        const isSelected = selectedDate && selectedDate.isSame(date, 'day');
-        const isBooked = date.format('YYYY-MM-DD') in bookingData;
-        const selectedDateFormatted = selectedDate ? selectedDate.format('YYYY-MM-DD') : null;
-        const isInRange =
-            selectedDateFormatted &&
-            bookingData[selectedDateFormatted] &&
-            date.isSameOrAfter(moment(bookingData[selectedDateFormatted].startDate), 'day') &&
-            date.isSameOrBefore(moment(bookingData[selectedDateFormatted].endDate), 'day');
-
+    const renderCalendarContainer = (containerNumber) => {
+        const isSelected = selectedDate === containerNumber;
         const containerStyle = {
-            width: '100vw',
+            width: '100%',
             height: '90px',
-            backgroundColor: 'lightgray',
+            backgroundColor: '#999',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'flex-start',
             position: 'relative',
             marginBottom: '5px',
         };
-
         const circleStyle = {
             width: '50px',
             height: '50px',
@@ -59,77 +291,110 @@ const BookingCalendar = () => {
             alignItems: 'center',
             justifyContent: 'center',
             backgroundColor: isSelected ? 'lightblue' : 'white',
-            border: '1px solid black',
             marginLeft: '10px',
+            cursor: 'pointer'
         };
-
-        const textStyle = {
-            fontWeight: isSelected ? 'bold' : 'normal',
-        };
-
         const bookingLineStyle = {
-            width: '100%',
-            height: '3px',
+            width: '0',
+            height: '15px',
+            borderRadius: '6px',
             backgroundColor: 'green',
-            position: 'absolute',
-            bottom: '0',
-            visibility: isInRange ? 'visible' : 'hidden',
+            visibility: bookingData[containerNumber] ? 'visible' : 'hidden',
+            left: 'calc(10px + 25px)',
+            transition: 'width 0.3s',
         };
 
-        const bookingDataContent = isBooked ? (
+        const bookingDataContent = bookingData[containerNumber] ? (
             <div style={{ position: 'absolute', bottom: '-20px' }}>
                 <p>
-                    Name: {bookingData[date.format('YYYY-MM-DD')].name} - Telefonnummer:{' '}
-                    {bookingData[date.format('YYYY-MM-DD')].phoneNumber} - Preis: {bookingData[date.format('YYYY-MM-DD')].price}
+                    Name: {bookingData[containerNumber].name} - Telefonnummer:{' '}
+                    {bookingData[containerNumber].phoneNumber} - Preis: {bookingData[containerNumber].price}
                 </p>
             </div>
         ) : null;
+        const bookingStartDate = bookingData[containerNumber] ? moment(bookingData[containerNumber].startDate, 'YYYY-MM-DD') : null;
+        const bookingEndDate = bookingData[containerNumber] ? moment(bookingData[containerNumber].endDate, 'YYYY-MM-DD') : null;
+
+        let bookingLineWidth = '0';
+
+        if (bookingStartDate && bookingEndDate) {
+            const bookingDuration = bookingEndDate.diff(bookingStartDate, 'days') + 1;
+            bookingLineWidth = `${bookingDuration * 50}px`;
+        }
 
         const handleContainerClick = () => {
-            setSelectedDate(date);
+            setSelectedDate(containerNumber);
             setShowModal(true);
         };
 
         return (
-            <Col key={index}>
+
+            <Row key={containerNumber} className="calendar-container">
                 <div style={containerStyle}>
-                    <div style={circleStyle} onClick={handleContainerClick}>
-                        <p style={textStyle}>{date.format('ddd')}</p>
+                    <div style={circleStyle} onClick={handleContainerClick}
+                        className={`container-circle ${isSelected ? "selected" : ""}`}
+
+                    >
+                        <p className="container-text">{containerNumber}</p>
                     </div>
                     {bookingDataContent}
-                    {isBooked && <div style={bookingLineStyle}></div>}
+                    {bookingData[containerNumber] && (
+                        <div
+                            style={{
+                                ...bookingLineStyle,
+                                width: bookingLineWidth,
+                            }}
+                        ></div>
+                    )}
                 </div>
-            </Col>
+            </Row>
+
         );
     };
 
     const renderStatusBars = () => {
         const statusBars = [];
-        let currentStatus = '';
-        let currentDate = moment().startOf('isoWeek');
+        let currentStatus = "";
         let isBooked = false;
 
-        while (currentDate.day() !== 0) {
-            const dateKey = currentDate.format('YYYY-MM-DD');
-            if (bookingData[dateKey]) {
+        for (let i = 1; i <= 7; i++) {
+            const containerNumber = i;
+            const containerKey = `container-${containerNumber}`;
+
+            if (bookingData[containerKey]) {
                 if (!isBooked) {
-                    currentStatus = `${bookingData[dateKey].name} - ${bookingData[dateKey].phoneNumber} - ${bookingData[dateKey].price}`;
+                    currentStatus = `${bookingData[containerKey].name} - ${bookingData[containerKey].phoneNumber} - ${bookingData[containerKey].price}`;
                     isBooked = true;
                 }
             } else {
                 if (isBooked) {
-                    statusBars.push(<div key={dateKey} className="status-bar" style={{ width: '10px' }}></div>);
+                    statusBars.push(
+                        <div
+                            key={containerKey}
+                            className="status-bar"
+                            style={{
+                                width: `${((i - 1) * 1) + 50}px`,
+                            }}
+                        ></div>
+                    );
                     isBooked = false;
                 }
             }
-            currentDate.add(1, 'day');
         }
 
         if (isBooked) {
-            statusBars.push(<div key={currentDate.format('YYYY-MM-DD')} className="status-bar" style={{ width: '10px' }}></div>);
+            statusBars.push(
+                <div
+                    key="container-8"
+                    className="status-bar"
+                    style={{
+                        width: `${(7 * 50) + 10}px`,
+                    }}
+                ></div>
+            );
         }
 
-        return statusBars;
+        return <div className="status-bars">{statusBars}</div>;
     };
 
     const renderBookingForm = () => {
@@ -140,8 +405,8 @@ const BookingCalendar = () => {
         const handleSubmit = (event) => {
             event.preventDefault();
             const formData = new FormData(event.target);
-            const startDate = moment(formData.get('startDate'), 'DD.MM.YYYY');
-            const endDate = moment(formData.get('endDate'), 'DD.MM.YYYY');
+            const startDate = moment(formData.get('startDate'), 'YYYY-MM-DD');
+            const endDate = moment(formData.get('endDate'), 'YYYY-MM-DD');
             const name = formData.get('name');
             const phoneNumber = formData.get('phoneNumber');
             const price = formData.get('price');
@@ -149,7 +414,7 @@ const BookingCalendar = () => {
         };
 
         return (
-            <Modal show={showModal} onHide={() => setShowModal(false)} fullscreen>
+            <Modal show={showModal} onHide={() => setShowModal(false)} centered>
                 <Modal.Header closeButton>
                     <Modal.Title>Buchung</Modal.Title>
                 </Modal.Header>
@@ -190,17 +455,14 @@ const BookingCalendar = () => {
     };
 
     const calendarContainers = [...Array(7)].map((_, index) => {
-        const date = moment().isoWeekday(index + 1);
-        return renderCalendarContainer(date, index);
+        const containerNumber = index + 1;
+        return renderCalendarContainer(containerNumber);
     });
 
     return (
-        <Container>
+        <Container fluid className="booking-calendar">
             <Row>{calendarContainers}</Row>
-            <Row>
-                {renderStatusBars()}
-                <div className="status-bar" style={{ flexGrow: 1 }}></div>
-            </Row>
+            {renderStatusBars()}
             {renderBookingForm()}
         </Container>
     );
--- frontend/src/components/belegungsPlan/WeeklyCalendar.jsx
+++ frontend/src/components/belegungsPlan/WeeklyCalendar.jsx
@@ -1,3 +1,76 @@
+// import React, { useState } from "react";
+// import moment from "moment";
+// import {
+//     Typography,
+//     Grid,
+//     IconButton,
+//     Button
+// } from "@mui/material";
+// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+// import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+// import BookingCalendar from "./BookingCalendar";
+
+// const WeeklyCalendar = () => {
+
+//     //Abkürzung der Wochentage
+//     const weekdays = moment.weekdaysMin()
+
+//     //Aktuelle Kalenderwoche
+//     const [currentWeek, setCurrentWeek] = useState(moment().isoWeek())
+
+//     //Pfeile Logik
+//     const handlePreviousWeek = () => {
+//         setCurrentWeek(prevWeek => prevWeek - 1)
+//     }
+//     const handleNextWeek = () => {
+//         setCurrentWeek(prevWeek => prevWeek + 1)
+//     }
+
+//     //zurück zur aktuellen Woche
+//     const handleCurrentWeek = () => {
+//         setCurrentWeek(moment().isoWeek())
+//     }
+
+//     // Startdatum der aktuellen Kalenderwoche ermitteln
+//     const startOfWeek = moment().isoWeek(currentWeek).startOf('isoWeek')
+
+//     //Wochentage generieren
+//     const weekDaysList = weekdays.map((day, index) => {
+//         const dayOfWeek = (index + 1) % 7;
+//         const date = startOfWeek.clone().add(index, 'days')
+
+//         return (
+//             <Grid item xs key={index}>
+//                 <Typography variant="h6">{weekdays[dayOfWeek]}</Typography>
+//                 <Typography variant="body1">{date.format('D MMM')}</Typography>
+//             </Grid>
+//         )
+
+//     })
+
+//     return (
+//         <div style={{ textAlign: 'center' }}>
+//             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '2vh' }}>
+//                 <IconButton onClick={handlePreviousWeek}>
+//                     <ChevronLeftIcon />
+//                 </IconButton>
+//                 <Typography variant="h6">KW  {currentWeek}</Typography>
+//                 <IconButton onClick={handleNextWeek}>
+//                     <ChevronRightIcon />
+//                 </IconButton>
+//             </div>
+//             <div style={{ marginTop: '10px' }}>
+//                 <Button variant="outlined" onClick={handleCurrentWeek}>Zurück Zur Aktuelle Woche</Button>
+//             </div>
+//             <Grid container spacing={2} marginTop={'2vh'}>
+//                 {weekDaysList}
+//             </Grid>
+//             <BookingCalendar />
+//         </div>
+//     )
+// };
+
+// export default WeeklyCalendar;
 import React, { useState } from "react";
 import moment from "moment";
 import {
@@ -6,76 +79,72 @@ import {
     IconButton,
     Button
 } from "@mui/material";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
-import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import BookingCalendar from "./BookingCalendar";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+import "./WeeklyCalendar.css"; // Importieren Sie die CSS-Datei
 
 const WeeklyCalendar = () => {
+    const weekdays = moment.weekdaysMin();
+    const [currentWeek, setCurrentWeek] = useState(moment().isoWeek());
 
-    //Abkürzung der Wochentage
-    const weekdays = moment.weekdaysMin()
-
-    //Aktuelle Kalenderwoche
-    const [currentWeek, setCurrentWeek] = useState(moment().isoWeek())
-
-    //Pfeile Logik
     const handlePreviousWeek = () => {
-        setCurrentWeek(prevWeek => prevWeek - 1)
-    }
+        setCurrentWeek(prevWeek => prevWeek - 1);
+    };
+
     const handleNextWeek = () => {
-        setCurrentWeek(prevWeek => prevWeek + 1)
-    }
+        setCurrentWeek(prevWeek => prevWeek + 1);
+    };
 
-    //zurück zur aktuellen Woche
     const handleCurrentWeek = () => {
-        setCurrentWeek(moment().isoWeek())
-    }
+        setCurrentWeek(moment().isoWeek());
+    };
 
-    // Startdatum der aktuellen Kalenderwoche ermitteln
-    const startOfWeek = moment().isoWeek(currentWeek).startOf('isoWeek')
+    const startOfWeek = moment().isoWeek(currentWeek).startOf("isoWeek");
 
-    //Aktuelles Datum und Wochentag ermitteln
-    // const currentDate = moment()
-    // const startOfWeek = currentDate.clone().startOf('isoWeek')
-    // const currentDayOfWeek = currentDate.isoWeekday()
-
-    //Wochentage generieren
     const weekDaysList = weekdays.map((day, index) => {
         const dayOfWeek = (index + 1) % 7;
-
-        const date = startOfWeek.clone().add(index, 'days')
-        // const dayOfWeek = date.isoWeekday()
-        // const dayOfWeek = (currentDayOfWeek + index) % 7
+        const date = startOfWeek.clone().add(index, "days");
 
         return (
-            <Grid item xs key={index}>
-                <Typography variant="h6">{weekdays[dayOfWeek]}</Typography>
-                <Typography variant="body1">{date.format('D MMM')}</Typography>
+            <Grid item xs key={index} className="calendar-day">
+                <Typography variant="h6" className="day-name">
+                    {weekdays[dayOfWeek]}
+                </Typography>
+                <Typography variant="body1" className="day-date">
+                    {date.format("D MMM")}
+                </Typography>
             </Grid>
-        )
-
-    })
+        );
+    });
 
     return (
-        <div style={{ textAlign: 'center' }}>
-            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                <IconButton onClick={handlePreviousWeek}>
+        <div className="weekly-calendar">
+            <div className="calendar-header">
+                <IconButton onClick={handlePreviousWeek} className="arrow-icon">
                     <ChevronLeftIcon />
                 </IconButton>
-                <Typography variant="h6">KW  {currentWeek}</Typography>
-                <IconButton onClick={handleNextWeek}>
+                <Typography variant="h6" className="week-number">
+                    KW {currentWeek}
+                </Typography>
+                <IconButton onClick={handleNextWeek} className="arrow-icon">
                     <ChevronRightIcon />
                 </IconButton>
             </div>
-            <div style={{ marginTop: '10px' }}>
-                <Button variant="outlined" onClick={handleCurrentWeek}>Zurück Zur Aktuelle Woche</Button>
+            <div>
+                <Button
+                    variant="outlined"
+                    onClick={handleCurrentWeek}
+                    className="booking-button"
+                >
+                    Zurück zur aktuellen Woche
+                </Button>
             </div>
-            <Grid container spacing={2} marginTop={'2vh'}>
+            <Grid container spacing={2} className="calendar-days">
                 {weekDaysList}
             </Grid>
-            {/* <BookingCalendar /> */}
         </div>
-    )
+    );
 };
 
 export default WeeklyCalendar;
