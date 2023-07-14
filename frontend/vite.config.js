2b9f68a6854d7365bf5ed010437be1284fc5c514
Ciprian Vlad <supercoder.cipi@gmail.com>
2023-07-14 14:21:09 +0200

    bug fix: test_deploy on render.com; added rollupOptions at vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['date-fns'],
+      external: ['date-fns', '@emotion/react'],
     },
   },
 })
