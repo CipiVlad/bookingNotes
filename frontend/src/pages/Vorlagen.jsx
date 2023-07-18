31fbdb697ccc37d21a7970a317cfde6841c18fff
Ciprian Vlad <supercoder.cipi@gmail.com>
2023-07-18 10:01:14 +0200

    deleted Booking.css, and .jsx

--- frontend/src/pages/Vorlagen.jsx
+++ frontend/src/pages/Vorlagen.jsx
@@ -3,21 +3,14 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-
 const Vorlagen = () => {
-    /**
-     *     <Link to={'/vorlagen/angebotsvorlage'}>Angebotsvorlage</Link>
-                    <Link to={'/vorlagen/mietvertragsvorlage'}>Mietvertragsvorlage</Link>
-                    <Link to={'/vorlagen/rechnungsvorlage'}>Rechnungsvorlage</Link>
-     */
-
-
     const arrayVorlagen = [
         {
             id: 1,
             name: 'Angebote',
             link: '/vorlagen/angebotsvorlage',
-            text: 'hier kannst du z.B. neue Mietvertragsvorlagen anlegen,verschiedene Varianten hinterlegen und einfach die Vorlage kopieren'
+            text: 'hier kannst du z.B. neue Mietvertragsvorlagen anlegen,verschiedene Varianten hinterlegen und einfach die Vorlage kopieren',
+
         },
         {
             id: 2,
@@ -35,21 +28,22 @@ const Vorlagen = () => {
     ]
 
     return (
-        <Row xs={1} md={2} className="g-4">
-            {arrayVorlagen.map((e, idx) => (
-                <Col key={idx}>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>{e.name}</Card.Title>
-                            <Card.Text>{e.text}</Card.Text>
-                            <Link to={e.link}>bearbeiten</Link>
-
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+        <>
+            <h2>Vorlagen</h2>
+            <Row xs={1} md={3} className="g-4">
+                {arrayVorlagen.map((e, idx) => (
+                    <Col key={idx}>
+                        <Card style={{ margin: '10vh auto', width: '250px', height: '250px' }}>
+                            <Card.Body>
+                                <Card.Title>{e.name}</Card.Title>
+                                <Card.Text>{e.text}</Card.Text>
+                                <Link to={e.link}>bearbeiten</Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
