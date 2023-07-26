d09ae21c7f421993e9d8462ce4765d8a97b2c740
CipiVlad <102032121+CipiVlad@users.noreply.github.com>
2023-07-26 16:43:41 +0200

    added Kundenstamm searchfield withouth logic, and client list

--- frontend/src/pages/Kundenstamm.jsx
+++ frontend/src/pages/Kundenstamm.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 const Kundenstamm = () => {
+    const [clients, setClients] = useState([]);
+
+
+    // API-Aufruf mit Axios, um die Buchungsdaten abzurufen
+    useEffect(() => {
+        loadingData()
+    }, []);
+
+    const loadingData = async () => {
+        await axios.get('http://localhost:3001/bookings')
+            .then(response => {
+                // Daten speichern
+                setClients(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error('Fehler beim Abrufen der Buchungsdaten:', error);
+            });
+    }
+
     return (
-        <div>Kundenstamm</div>
-    )
+        <>
+            <Stack spacing={2} sx={{ width: 300, margin: ' 0 auto' }}>
+                <TextField
+                    label="Suche nach Kunde"
+                />
+            </Stack>
+
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Emailadresse</th>
+                        <th>Telefonnummer</th>
+                    </tr>
+                </thead>
+                {clients.map((client, index) => (
+                    <tbody key={index}>
+                        <tr >
+                            <td>{client.id}</td>
+                            <td>{client.name}</td>
+                            <td>
+                                <a href="mailto:">{client.emailAddress}</a>
+                            </td>
+                            <td>
+                                <a href="tel:">{client.phoneNumber}</a>
+                            </td>
+                        </tr>
+                    </tbody >
+                ))}
+            </Table>
+        </>
+    );
 }
 
-export default Kundenstamm
+export default Kundenstamm;
